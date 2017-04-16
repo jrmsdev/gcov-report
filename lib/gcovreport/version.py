b35f29a1955c62b39079da844778fd77f0a579a8
Jeremías <jrmsdev@gmail.com>
2017-04-16 02:09:28 -0300

    rename get_string

--- lib/gcovreport/version.py
+++ lib/gcovreport/version.py
@@ -2,11 +2,11 @@ VMAJOR = 0
 VMINOR = 1
 VPATCH = 0
 
-def get ():
+def get_string ():
     v = "%d.%d" % (VMAJOR, VMINOR)
     if VPATCH > 0:
         v += ".%d" % VPATCH
     return v
 
 def printv (appname = 'gcov-report'):
-    print ("%s v%s" % (appname, get ()))
+    print ("%s v%s" % (appname, get_string ()))

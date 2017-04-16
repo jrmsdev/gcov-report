2948b105d45ba618bb8174686a3ef0f8433fe01f
Jeremías <jrmsdev@gmail.com>
2017-04-16 00:35:52 -0300

    initial commit

---
+++ bin/gcov-report.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+from os import path
+
+lib_check = path.join (path.dirname (__file__),
+        '..', 'lib', 'gcovreport', '__init__.py')
+
+if path.isfile (lib_check):
+    sys.path.insert (0, path.dirname (path.dirname (lib_check)))
+    from gcovreport import cmd
+else:
+    try:
+        from gcovreport import cmd
+    except ImportError:
+        print ("ERR: could not import gcovreport python lib", file = sys.stderr)
+        sys.exit (1)
+sys.exit (cmd.main ())

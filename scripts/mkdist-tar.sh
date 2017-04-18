96562dc74a97062c715e12a22748223e0da892a1
Jeremías <jrmsdev@gmail.com>
2017-04-18 05:47:38 -0300

    fix creating dist tar

--- scripts/mkdist-tar.sh
+++ scripts/mkdist-tar.sh
@@ -3,7 +3,7 @@
 ver="$(./scripts/get-version.sh | cut -d' ' -f2)"
 cd dist/work || exit 1
 
-tar_file=$(realpath ../gcov-report-${ver}.txz)
+tar_file=$(realpath $(pwd)/..)/gcov-report-${ver}.txz
 tar -cJf $tar_file `find . -type f | sort`
 echo "$tar_file done"
 

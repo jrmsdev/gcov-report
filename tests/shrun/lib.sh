b279fca011b671e823ad1e0f2841d984e77615bb
Jeremías <jrmsdev@gmail.com>
2017-04-18 00:38:22 -0300

    MYNAME and TMPDIR env vars for shrun usage

--- tests/shrun/lib.sh
+++ tests/shrun/lib.sh
@@ -1,16 +1,18 @@
 GCOV_REPORT="$(realpath ../bin/gcov-report.py)"
 COVCMD="${COVERAGE_CMD}"
 INITD="$(pwd)"
+MYNAME="shrun/$(basename $0 .sh)"
+TMPDIR="${INITD}/testdata/${MYNAME}.tmpdir"
 
-echo "shrun $(basename $0)" # ensure at least one line of output
+echo "$MYNAME" # ensure at least one line of output
 
 test -x $GCOV_REPORT || {
-    echo "ERR: $GCOV_REPORT script not found"
+    echo "ERR ${MYNAME}: $GCOV_REPORT script not found"
     exit 1
 }
 
-test -d testdata || {
-    echo "ERR: testdata dir not found"
+test -d ./testdata || {
+    echo "ERR ${MYNAME}: testdata dir not found"
     exit 1
 }
 
@@ -19,5 +21,5 @@ run_gcov_report() {
     if test "x$COVCMD" != "x"; then
         covcmd="${INITD}/$COVCMD"
     fi
-    $covcmd $GCOV_REPORT --test-mode  --htmldir=testdata --gcovdir=testdata $@
+    $covcmd $GCOV_REPORT --test-mode  --htmldir=t$TMPDIR --gcovdir=$TMPDIR $@
 }

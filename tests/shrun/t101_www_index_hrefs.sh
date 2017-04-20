135aeac4e594b937e4dd1e3f2f00c0d12f1f68aa
Jeremías <jrmsdev@gmail.com>
2017-04-20 05:45:34 -0300

    test www index hrefs

---
+++ tests/shrun/t101_www_index_hrefs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+shrunlib="$(dirname $0)/lib.sh"
+test -s $shrunlib || {
+    echo "ERR: $shrunlib lib not found"
+    exit 1
+}
+
+SHRUN_DEBUG=false
+. $shrunlib
+
+mkdir -p $TMPDIR || exit 1
+cd $TMPDIR || exit 1
+
+cp -v ../../../t???_*.c.gcov ./
+
+run_gcov_report >/dev/null
+
+index_html=${TMPDIR}/index.html
+if test -s $index_html; then
+    cat $index_html | grep -F 'href'
+else
+    echo "${index_html}: file not found"
+fi
+
+cd $INITD
+rm -rf $TMPDIR
+exit 0
